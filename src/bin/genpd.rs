f2cb5ea094571c0f15b2e1ef7b646ef5a8d462ab
sriram98v <sriram.98v@gmail.com>
2024-12-09 11:03:55 -0600

    add all_min and all_max option

--- src/bin/genpd.rs
+++ src/bin/genpd.rs
@@ -46,6 +46,24 @@ fn main() -> Result<()>{
                                 .required(true)
                                 .value_parser(clap::value_parser!(usize)),
                         ),
+                )
+                .subcommand(
+                    Command::new("all_max")
+                        .about("Compute maxPD")
+                        .arg(
+                            arg!(-f --file <TREE_FILE> "Input Tree File")
+                                .required(true)
+                                .value_parser(clap::value_parser!(String)),
+                        )
+                )
+                .subcommand(
+                    Command::new("all_min")
+                        .about("Compute maxPD")
+                        .arg(
+                            arg!(-f --file <TREE_FILE> "Input Tree File")
+                                .required(true)
+                                .value_parser(clap::value_parser!(String)),
+                        )
                 ),
 
         )
@@ -65,8 +83,16 @@ fn main() -> Result<()>{
                             .required(true)
                             .value_parser(clap::value_parser!(String)),
                     ),
+                )
+                .subcommand(
+                    Command::new("all")
+                        .about("Compute maxPD")
+                        .arg(
+                            arg!(-f --file <TREE_FILE> "Input Tree File")
+                                .required(true)
+                                .value_parser(clap::value_parser!(String)),
+                        )
                 ),
-
         )
         .about("CLI tool for quick tree operations")
         .get_matches();
@@ -106,6 +132,43 @@ fn main() -> Result<()>{
                     );
                     // dbg!("{}", tree);
                 },
+                Some(("all_min", min_pd)) => {
+                    let mut tree_file =
+                        File::open(min_pd.get_one::<String>("file").expect("required"))?;
+                    // let num_taxa = min_pd.get_one::<usize>("num_taxa").expect("required");
+                    let mut trees = String::new();
+
+                    tree_file.read_to_string(&mut trees).unwrap();
+                    let tree_string = trees.split("\n").collect_vec()[0];
+                    let mut tree = SimpleRootedTree::from_newick(tree_string.as_bytes())?;
+                    if !tree.is_binary(){
+                        binarize_tree(&mut tree);
+                    }
+                    // let num_taxa = match *num_taxa==0{
+                    //     true => {println!("setting k to n");tree.num_taxa()},
+                    //     false => *num_taxa as usize,
+                    // };
+                    let tree_taxa: usize = tree.num_taxa();
+
+                    let tree_pd = TreePD::new(&tree);
+                    for num_taxa in 3..tree_taxa+1{
+                        println!(
+                            "k: {}\nminPD: {}\nnormalized minPD: {}\nminPD set:{}\nnormalized minPD set:{}\n",
+                            num_taxa,
+                            tree_pd.get_minPD(num_taxa.clone()),
+                            tree_pd.get_norm_minPD(num_taxa.clone()),
+                            tree_pd
+                                .get_minPD_taxa_set(num_taxa.clone())
+                                .map(|x| tree.get_node_taxa(x).unwrap())
+                                .join(","),
+                            tree_pd
+                                .get_norm_minPD_taxa_set(num_taxa.clone())
+                                .map(|x| tree.get_node_taxa(x).unwrap())
+                                .join(",")
+                        );    
+                    }
+                    // dbg!("{}", tree);
+                },
                 Some(("max", max_pd)) => {
                     let mut tree_file =
                         File::open(max_pd.get_one::<String>("file").expect("required"))?;
@@ -138,7 +201,45 @@ fn main() -> Result<()>{
                             .join(",")
                     );
                     // dbg!("{}", tree);
-                }
+                },
+                Some(("all_max", max_pd)) => {
+                    let mut tree_file =
+                        File::open(max_pd.get_one::<String>("file").expect("required"))?;
+                    // let n_taxa = max_pd.get_one::<usize>("num_taxa").expect("required");
+                    let mut trees = String::new();
+
+                    tree_file.read_to_string(&mut trees).unwrap();
+                    let tree_string = trees.split("\n").collect_vec()[0];
+                    let mut tree = SimpleRootedTree::from_newick(tree_string.as_bytes())?;
+                    if !tree.is_binary(){
+                        binarize_tree(&mut tree);
+                    }
+                    // let num_taxa = match *n_taxa==0{
+                    //     true => {println!("setting k to n");tree.num_taxa()},
+                    //     false => *n_taxa,
+                    // };
+
+                    let tree_taxa: usize = tree.num_taxa();
+
+                    let tree_pd = TreePD::new(&tree);
+                    for num_taxa in 3..tree_taxa+1{
+                        println!(
+                            "k: {}\nmaxPD: {}\nnormalized maxPD: {}\nmaxPD set:{}\nnormalized maxPD set:{}\n",
+                            num_taxa,
+                            tree_pd.get_maxPD(num_taxa.clone()),
+                            tree_pd.get_norm_maxPD(num_taxa.clone()),
+                            tree_pd
+                                .get_maxPD_taxa_set(num_taxa.clone())
+                                .map(|x| tree.get_node_taxa(x).unwrap())
+                                .join(","),
+                            tree_pd
+                                .get_norm_maxPD_taxa_set(num_taxa.clone())
+                                .map(|x| tree.get_node_taxa(x).unwrap())
+                                .join(",")
+                        );
+                    }
+                    // dbg!("{}", tree);
+                },
 
                 _ => println!("No valid PD metric chosen! Refer help page (-h flag)"),
             }
